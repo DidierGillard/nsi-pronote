9ea7624f2d24944131a3d6693362080bf9c21e26
Didier Gillard <didier.gillard@univ-reims.fr>
2024-11-28 12:55:14 +0100

    Etape 4.1 : ajout tests de la fin des fonctionnalités de Eleve

--- tests/test_eleve.py
+++ tests/test_eleve.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.Eleve import Eleve
 from src.Note import Note
+from src.Matiere import Matiere
 
 
 @pytest.fixture
@@ -15,21 +16,64 @@ def eleve():
     ])
 
 
-def test_liste_notes_matieres(eleve):
-    assert eleve.liste_notes_matiere(1) == [18, 6, 0], "liste seulement les notes non null de la matière"
+@pytest.fixture
+def eleve_sans_note():
+    return Eleve(1, "Eleve1", [])
 
-def test_liste_notes_matieres_vide(eleve):
-    assert eleve.liste_notes_matiere(3) == []
 
-def test_a_note_dans_matiere(eleve):
-    assert eleve.a_note_dans_matiere(1) == True
+@pytest.fixture
+def matieres():
+    return [
+        Matiere(identifiant=1, nom="NSI", coef=2),
+        Matiere(identifiant=2, nom="Mathématique", coef=4),
+        Matiere(identifiant=3, nom="Histoire", coef=1),
+    ]
 
-def test_a_note_dans_matiere_sans_note(eleve):
-    assert eleve.a_note_dans_matiere(3) == False
 
-def test_moyenne_correct(eleve):
-    assert eleve.moyenne_matiere(1) == 8.0
+class TestListeNotesMatieres:
+    def test_liste_correct(self, eleve):
+        assert eleve.liste_notes_matiere(1) == [18, 6, 0], "liste seulement les notes non null de la matière"
 
-def test_moyenne_sans_note_raise_ValueError(eleve):
-    with pytest.raises(ValueError):
-        eleve.moyenne_matiere(3)
+    def test_liste_vide(self, eleve):
+        assert eleve.liste_notes_matiere(3) == []
+
+
+class TestANoteDansMatiere:
+    def test_vrai(self, eleve):
+        assert eleve.a_note_dans_matiere(1) == True
+
+    def test_faux(self, eleve):
+        assert eleve.a_note_dans_matiere(3) == False
+
+
+class TestMoyenneMatiere:
+    def test_moyenne_correct(self, eleve):
+        assert eleve.moyenne_matiere(1) == 8.0
+
+    def test_moyenne_sans_note_raise_ValueError(self, eleve):
+        with pytest.raises(ValueError):
+            eleve.moyenne_matiere(3)
+
+
+class TestSommePondereeMoyennes:
+    def test_somme_correcte(self, eleve, matieres):
+        assert eleve.somme_ponderee_moyennes(matieres) == 16
+
+    def test_sans_note_est_None(self, eleve_sans_note, matieres):
+        assert eleve_sans_note.somme_ponderee_moyennes(matieres) is None
+
+
+class TestSommePonderationsMatieres:
+    def test_somme_correct(self, eleve, matieres):
+        assert eleve.somme_ponderations_matieres(matieres) == 6
+
+    def test_sans_note_est_None(self, eleve_sans_note, matieres):
+        assert eleve_sans_note.somme_ponderations_matieres(matieres) is None
+
+
+class TestMoyenneGenerale:
+    def test_moyenne_correcte(self, eleve, matieres):
+        assert eleve.moyenne_generale(matieres) == 16 / 6
+
+    def test_moyenne_sans_note_est_None(self, eleve_sans_note, matieres):
+        assert eleve_sans_note.moyenne_generale(matieres) is None
