d22a51d89220746d94ab638e45f0aced84ab9b75
Didier Gillard <didier.gillard@univ-reims.fr>
2024-11-28 12:29:30 +0100

    Etape 4.1 : ajout de la fin des fonctionnalités de Eleve

--- src/Eleve.py
+++ src/Eleve.py
@@ -1,5 +1,6 @@
 from typing import List
 from src.Note import Note
+from src.Matiere import Matiere
 
 class Eleve:
     id: int
@@ -34,3 +35,35 @@ class Eleve:
             raise ValueError
 
         return sum(notes) / nb_notes
+
+    """ :return la somme pondérée des moyennes des matieres de l'éléve. None si l'éléve n'a aucune note """
+    def somme_ponderee_moyennes(self, matieres: List[Matiere]) -> float|None:
+        somme = 0
+        a_note = False
+        for matiere in matieres:
+            try:
+                moy = self.moyenne_matiere(matiere.id)
+                somme += moy * matiere.coef
+                a_note = True
+            except ValueError:
+                pass
+
+        return somme if a_note else None
+
+    """ :return la somme des coéfficients des matières pour lesquelles l'éléve à au moins une note non null """
+    def somme_ponderations_matieres(self, matieres: List[Matiere]) -> float|None:
+        somme = 0
+        a_note = False
+        for matiere in matieres:
+            if self.a_note_dans_matiere(matiere.id):
+                somme += matiere.coef
+                a_note = True
+
+        return somme if a_note else None
+
+    """ :return la moyenne générale de l'éléve. None si l'éléve n'a aucune note """
+
+    def moyenne_generale(self, matieres: List[Matiere]) -> float|None:
+        somme_ponderee = self.somme_ponderee_moyennes(matieres)
+        ponderations = self.somme_ponderations_matieres(matieres)
+        return somme_ponderee / ponderations if somme_ponderee is not None else None
