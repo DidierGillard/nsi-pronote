a25c2befd10b3c821d93f9461d4016ea2b969c42
Didier Gillard <didier.gillard@univ-reims.fr>
2024-11-28 12:16:04 +0100

    Etape 3.1 : ajout tests et fonctionnalités avec bug

--- src/Eleve.py
+++ src/Eleve.py
@@ -18,3 +18,17 @@ class Eleve:
             for note in self.notes
             if note.matiere == matiere and note.valeur is not None
         ]
+
+    """ :return vrai si l'élève à au moins une note dans la matière passée en paramètre, ou faux dans le cas contraire """
+
+    def a_note_dans_matiere(self, matiere: int) -> bool:
+        return len(self.liste_notes_matiere(matiere)) != 0
+
+    """ :return la moyenne arithmétique des notes non nulles de la matière passée en paramètre """
+    """ :exception ValueError si l'atudiant n'a aucune note dans cette matière """
+
+    def moyenne_matiere(self, matiere: int) -> float:
+        notes = self.liste_notes_matiere(matiere)
+        nb_notes = len(self.liste_notes_matiere(matiere))
+
+        return sum(notes) / nb_notes
--- tests/test_eleve.py
+++ tests/test_eleve.py
@@ -18,6 +18,18 @@ def eleve():
 def test_liste_notes_matieres(eleve):
     assert eleve.liste_notes_matiere(1) == [18, 6, 0], "liste seulement les notes non null de la matière"
 
-
 def test_liste_notes_matieres_vide(eleve):
     assert eleve.liste_notes_matiere(3) == []
+
+def test_a_note_dans_matiere(eleve):
+    assert eleve.a_note_dans_matiere(1) == True
+
+def test_a_note_dans_matiere_sans_note(eleve):
+    assert eleve.a_note_dans_matiere(3) == False
+
+def test_moyenne_correct(eleve):
+    assert eleve.moyenne_matiere(1) == 8.0
+
+def test_moyenne_sans_note_raise_ValueError(eleve):
+    with pytest.raises(ValueError):
+        eleve.moyenne_matiere(3)
