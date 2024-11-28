5aa8ce615a0add60ff8f6f3c6dfbd1778bd047ff
Didier Gillard <didier.gillard@univ-reims.fr>
2024-11-28 12:08:41 +0100

    Etape 2.3 : refactor tests avec fixtures

--- tests/test_eleve.py
+++ tests/test_eleve.py
@@ -1,18 +1,23 @@
+import pytest
+
 from src.Eleve import Eleve
 from src.Note import Note
 
 
-def test_liste_notes_matieres():
-    eleve = Eleve(1, "Eleve1", [
+@pytest.fixture
+def eleve():
+    return Eleve(1, "Eleve1", [
         Note(1, 18),
         Note(1, None),
         Note(2, 0),
         Note(1, 6),
         Note(1, 0)
     ])
+
+
+def test_liste_notes_matieres(eleve):
     assert eleve.liste_notes_matiere(1) == [18, 6, 0], "liste seulement les notes non null de la matière"
 
 
-def test_liste_notes_matieres_vide():
-    eleve = Eleve(1, "Eleve1", [])
+def test_liste_notes_matieres_vide(eleve):
     assert eleve.liste_notes_matiere(3) == []
