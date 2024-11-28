7bf1d9478e9cc0266f624cbf157239ba6613c3ca
Didier Gillard <didier.gillard@univ-reims.fr>
2024-11-28 11:32:04 +0100

    Etape 1.2 : ajout des tests de structure

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -3,10 +3,20 @@ from src.Matiere import Matiere
 from src.Eleve import Eleve
 
 def test_class_Note():
-    assert False
+    note = Note(matiere=2, valeur=12.5)
+    assert note.matiere == 2
+    assert note.valeur == 12.5
 
 def test_class_Matiere():
-    assert False
+    mat = Matiere(identifiant=2, nom="histoire", coef=3)
+    assert mat.id == 2
+    assert mat.nom == "histoire"
+    assert mat.coef == 3
 
 def test_class_Eleve():
-    assert False
+    note1 = Note(matiere=2, valeur=12.5)
+    note2 = Note(matiere=2, valeur=None)
+    eleve = Eleve(identifiant=2, nom="Dupont", notes=[note1, note2])
+    assert eleve.id == 2
+    assert eleve.nom == "Dupont"
+    assert eleve.notes == [note1, note2]
